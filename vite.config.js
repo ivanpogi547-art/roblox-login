f025d91429a4fe8a70b7a49571ed0562daca908e
godwinn <godwinngallego@gmail.com>
2025-10-23 18:55:20 +0800

    fix: add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/roblox-login/', // 👈 this line fixes the blank page
   plugins: [react()],
 })
